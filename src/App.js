d861850c895f0131d2153332b4e8cc1a549d21a6
krbex <aidan.krbec5@gmail.com>
2022-12-28 21:45:53 -0600

    navigation section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react';
-// TODO: Add a comment explaining what this import statement is doing
-import HelloReact from './components/HelloReact';
+import React from "react";
+import Navigation from "./components/Navigation";
 
-// TODO: Add a comment explaining the purpose of the App component
 function App() {
-  return <HelloReact />;
+  return <Navigation />;
 }
 
 export default App;
